9396eef265465d18df64868d6ce94c5e31e69a04
Albin David C <albindavidc007@gmail.com>
2024-12-19 07:55:44 +0530

    updated status of the applicaiton

--- src/app/tasks/tasks.services.ts
+++ src/app/tasks/tasks.services.ts
@@ -1,22 +1,28 @@
-import { Inject, Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
-
+import { Inject, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
+export class TasksService {
+  tasks = signal<Task[]>([]);
 
-export class TasksService{
-    tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
 
-    allTasks = this.tasks.asReadonly();
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 
-    addTask(taskData: {title: string; description: string}){
-        const newTask: Task = {
-            ...taskData,
-            id: Math.random().toString(),
-            status: 'OPEN'
-        };
-        this.tasks.update((oldTasks) => [...oldTasks, newTask])
-    }
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
 }
